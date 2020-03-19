a8bd8df12bca847f04a9b8de038f284fd78e40a3
anisimovv <kirill.anisimovv@gmail.com>
2020-03-19 17:02:25 +0200

    Updates

---
+++ precache-manifest.744a2b9bc8d10accbaffdcae33745bdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a2d6e0b968458d8005ca9317533c9e",
+    "url": "/weired-robots/index.html"
+  },
+  {
+    "revision": "0f9f04fa04de123e8a95",
+    "url": "/weired-robots/static/css/main.a467e1dc.chunk.css"
+  },
+  {
+    "revision": "73cbeced8fd16fe0f3cc",
+    "url": "/weired-robots/static/js/2.20f71596.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weired-robots/static/js/2.20f71596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f9f04fa04de123e8a95",
+    "url": "/weired-robots/static/js/main.f5a6c470.chunk.js"
+  },
+  {
+    "revision": "14292515d4e94f009f9b",
+    "url": "/weired-robots/static/js/runtime-main.169e8815.js"
+  }
+]);
